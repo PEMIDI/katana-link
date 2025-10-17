9729b271d50a69c8795e8a8744d8159064ce56ef
pemidi <rashidi.peyman@gmail.com>
2025-10-17 21:22:46 +0330

    Introduce `Link` model and `BaseModel` for database schema definition

---
+++ app/models/basemodel.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+Base = DeclarativeBase
+
+timestamp_created_at = Annotated[
+    datetime,
+    mapped_column(nullable=False, server_default=func.now())
+]
+
+timestamp_updated_at = Annotated[
+    datetime,
+    mapped_column(nullable=False, server_default=func.now(), onupdate=func.now())
+]
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id: Mapped[Annotated[int, mapped_column(primary_key=True, index=True)]]
+    created_time = Mapped[timestamp_created_at]
+    updated_at = Mapped[timestamp_updated_at]
