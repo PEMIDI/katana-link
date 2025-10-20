1ea43b829110c63b07611e8ca71a9db65c4b9d52
pemidi <rashidi.peyman@gmail.com>
2025-10-20 18:27:38 +0330

    Add GET endpoint for redirecting short URLs, refactor URL services and repository logic, and modify schema/migration to allow nullable `short_url`.

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,9 +1,10 @@
-from .health import router as health_router  # re-export
-
 from fastapi import APIRouter
 
+from .health import router as health_router
 from .post_short_url import router as post_short_url
+from .get_long_url import router as get_short_url
 
 api_router = APIRouter()
 api_router.include_router(health_router, tags=["health"])
-api_router.include_router(post_short_url,prefix='/create-link', tags=["post_short_url"])
+api_router.include_router(post_short_url, prefix='/create-link', tags=["post_short_url"])
+api_router.include_router(get_short_url, prefix='/redirect-link', tags=["get_long_url"])
--- app/api/routes/post_short_url.py
+++ app/api/routes/post_short_url.py
@@ -1,9 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.deps import get_db
+from schemas.link_dto import LinkCreate
+from services.link_service import LinkService
 
 router = APIRouter()
 
+post_short_url_summary = "Create a short link"
 
-@router.post("/short_url")
-async def post_short_url():
-    print("hello world")
-    return None
+
+def get_link_service(db: AsyncSession = Depends(get_db)) -> LinkService:
+    return LinkService(db)
+
+
+@router.post(
+    "/short_url",
+    response_model=None,
+    status_code=status.HTTP_201_CREATED,
+    summary=post_short_url_summary,
+
+)
+async def post_short_url(link: LinkCreate, link_service: LinkService = Depends(get_link_service), ):
+    # TODO if exists, just return the short url
+
+
+
+    # TODO if not exists, create a new short url
+    link = await link_service.create_link(data=link)
+    return link
