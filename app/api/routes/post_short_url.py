93744d0f07ddec0d639dc83f325cbd4fc81ea0ed
pemidi <rashidi.peyman@gmail.com>
2025-10-21 10:58:33 +0330

    Refactor link creation logic to return tuple with creation status and update POST endpoint to handle existing links

--- app/api/routes/post_short_url.py
+++ app/api/routes/post_short_url.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.deps import get_db
-from app.schemas.link_dto import LinkCreate
+from app.schemas.link_dto import LinkCreate, LinkRead
 from app.services.link_service import LinkService
 
 router = APIRouter()
@@ -16,16 +16,14 @@ def get_link_service(db: AsyncSession = Depends(get_db)) -> LinkService:
 
 @router.post(
     "/short_url",
-    response_model=None,
-    status_code=status.HTTP_201_CREATED,
+    response_model=LinkRead,
     summary=post_short_url_summary,
-
 )
-async def post_short_url(link: LinkCreate, link_service: LinkService = Depends(get_link_service), ):
-    # TODO if exists, just return the short url
-
-
-
-    # TODO if not exists, create a new short url
-    link = await link_service.create_link(data=link)
-    return link
+async def post_short_url(
+    link: LinkCreate,
+    response: Response,
+    link_service: LinkService = Depends(get_link_service),
+):
+    created_link, is_created = await link_service.create_link(data=link)
+    response.status_code = status.HTTP_201_CREATED if is_created else status.HTTP_200_OK
+    return LinkRead(short_link=created_link.short_url, long_link=created_link.long_url)
